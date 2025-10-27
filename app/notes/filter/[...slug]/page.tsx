31e1195075b214912f120b70c5647377a7a9d54e
helen-akateva <akateva17@gmail.com>
2025-10-27 15:26:43 +0200

    add

---
+++ app/notes/filter/[...slug]/page.tsx
@@ -0,0 +1,32 @@
+import { fetchNotes } from "@/lib/api";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import NotesClient from "./Notes.client";
+import { NoteTag } from "@/types/note";
+
+interface NotesPageProps {
+  params: Promise<{ slug: [NoteTag | "all"] }>;
+}
+
+const NotesPage = async ({ params }: NotesPageProps) => {
+  const queryClient = new QueryClient();
+
+  const { slug } = await params;
+  const tag = slug[0] === "all" ? undefined : slug[0];
+
+  await queryClient.prefetchQuery({
+    queryKey: ["notes", tag],
+    queryFn: () => fetchNotes({ tag }),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotesClient tag={tag} />
+    </HydrationBoundary>
+  );
+};
+
+export default NotesPage;
